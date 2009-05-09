ebfbe3447edae50c019ef254f767a1f0d64e4fc8
Chris Williams <chris@bingosnet.co.uk>
2009-05-09 16:20:28 +0100

    Started to build up the API

--- lib/POE/Component/Github.pm
+++ lib/POE/Component/Github.pm
@@ -3,6 +3,7 @@ package POE::Component::Github;
 use strict;
 use warnings;
 use POE::Component::Client::HTTP;
+use Algorithm::FloodControl;
 use vars qw($VERSION);
 
 $VERSION = '0.02';
@@ -33,7 +34,7 @@ has _http_alias => (
 );
 
 sub START {
-  my ($self) = @_;
+  my $self = shift;
   $self->_http_alias( join '-', __PACKAGE__, $self->get_session_id );
   $poe_kernel->refcount_increment( $self->get_session_id, __PACKAGE__ );
   POE::Component::Client::HTTP->spawn(
@@ -42,6 +43,43 @@ sub START {
   );
   return;
 }
+
+event shutdown => sub {
+  my ($kernel,$self) = @_[KERNEL,OBJECT];
+  $kernel->refcount_decrement( $self->get_session_id, __PACKAGE__ );
+  $kernel->post( $self->_http_alias, 'shutdown' );
+  return;
+};
+
+event user => sub {
+  my ($kernel,$self,$cmd) = @_[KERNEL,OBJECT,ARG0];
+  return;
+};
+
+event issues => sub {
+  my ($kernel,$self,$cmd) = @_[KERNEL,OBJECT,ARG0];
+  return;
+};
+
+event repos => sub {
+  my ($kernel,$self,$cmd) = @_[KERNEL,OBJECT,ARG0];
+  return;
+};
+
+event commits => sub {
+  my ($kernel,$self,$cmd) = @_[KERNEL,OBJECT,ARG0];
+  return;
+};
+
+event object => sub {
+  my ($kernel,$self,$cmd) = @_[KERNEL,OBJECT,ARG0];
+  return;
+};
+
+event network => sub {
+  my ($kernel,$self,$cmd) = @_[KERNEL,OBJECT,ARG0];
+  return;
+};
 
 no MooseX::POE;
 
--- t/01_instance.t
+++ t/01_instance.t
@@ -1,10 +1,12 @@
 use strict;
 use warnings;
-use Test::More tests => 1;
+use Test::More tests => 2;
 use POE qw(Component::Github);
 
 my $github = POE::Component::Github->new();
 isa_ok( $github, 'POE::Component::Github');
 
+$poe_kernel->post( $github->get_session_id, 'shutdown' );
+
 $poe_kernel->run();
-exit 0;
+pass("Okay the kernel returned");
